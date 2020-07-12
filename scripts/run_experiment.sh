dbd0a0363b1f8adfc9ed7f45f3f40977c9fc8825
RchalYang <ruihanyang97@gmail.com>
2020-07-11 23:05:45 -0700

    add sleep in scripts to avoid overwrite

--- scripts/run_experiment.sh
+++ scripts/run_experiment.sh
@@ -8,9 +8,12 @@ config=$6
 id=$7
 
 $pythonpath run.py --config $config --log_dir $logdir --seed $SEED1 --id $id &
+sleep 10s
 
 $pythonpath run.py --config $config --log_dir $logdir --seed $SEED2 --id $id &
+sleep 10s
 
 $pythonpath run.py --config $config --log_dir $logdir --seed $SEED3 --id $id &
+sleep 10s
 
 wait
