ab0b9b5c262d0c6b147da1bb4fc911dd8cf07d0a
RchalYang <ruihanyang97@gmail.com>
2020-07-11 23:11:34 -0700

    fix script

--- scripts/run_experiment.sh
+++ scripts/run_experiment.sh
@@ -1,6 +1,6 @@
 SEED1=$1
-SEED1=$2
-SEED1=$3
+SEED2=$2
+SEED3=$3
 
 pythonpath=$4
 logdir=$5
@@ -8,12 +8,12 @@ config=$6
 id=$7
 
 $pythonpath run.py --config $config --log_dir $logdir --seed $SEED1 --id $id &
-sleep 10s
+# sleep 10s
 
 $pythonpath run.py --config $config --log_dir $logdir --seed $SEED2 --id $id &
-sleep 10s
+# sleep 10s
 
 $pythonpath run.py --config $config --log_dir $logdir --seed $SEED3 --id $id &
-sleep 10s
+# sleep 10s
 
 wait
